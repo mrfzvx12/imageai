b21823463cac545fff4f4db9821adae2250dd622
Ma'ruf Ma'arij <72728486+mrfzvx12@users.noreply.github.com>
2022-12-20 11:12:30 +0700

    Create index.d.ts

---
+++ dits/Constants/index.d.ts
@@ -0,0 +1,2 @@
+export declare const urlRegex: RegExp;
+export declare const userAgent = "Mozilla/5.0 (X11; Linux x86_64; rv:108.0) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/107.0.0.0 Safari/537.36";
